be544943ec666497c734f3d92522f0d9a508bf27
Terebi_V2 <41145193+RemoteScript@users.noreply.github.com>
2023-01-03 23:12:51 -0500

    Create BackupConfig.lua

---
+++ BackupConfig.lua
@@ -0,0 +1,28 @@
+--//Settings
+
+local Settings = {
+    Region = "Midnight", -- Dive starting region
+    LeaveAtRegion = "Midnight", -- nil for completing until Dreams, put your desired area if you want to leave at specify area example "Shallows"
+    Difficulty = 400,
+    Special = true, 
+    NextRegion = flase, -- Keep it true so you can synced all the items from the dive
+    AutoSell = true,
+    TempFarm = true, -- Pillar Farm and SYNC farm
+    RarityFilter = "Alpha" -- Examples: "Pristine" Ignores Pristine and above, "Legendary" Ignores Legendary and above. Filters: Pristine, Experimental, Legendary, Alpha, Omega, Core
+}
+
+--//Services
+local HttpService = game:GetService("HttpService");
+
+function saveSettings()
+    local savedTable = HttpService:JSONEncode(Settings);
+
+    writefile("NK_Settings.json", savedTable);
+    Settings = HttpService:JSONDecode(readfile("NK_Settings.json"));
+end
+
+-- Call saveSettings function
+saveSettings();
+
+-- Load the code from the specified URL
+loadstring(game:HttpGet("https://pastebin.com/raw/FaTmzbRJ"))();
